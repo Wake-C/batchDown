3ad200fedc8deab622b141a419d13ac88d4e6974
zhanbing <981506076@qq.com>
2018-04-24 20:21:01 +0800

    run 163

--- src/main/java/runDown163_kehanxueyuan_finance.java
+++ src/main/java/runDown163_kehanxueyuan_finance.java
@@ -4,8 +4,9 @@ public class runDown163_kehanxueyuan_finance {
 
     public static void main( String[] args ) throws IOException {
 	    String COURSEURL[] ={
-			    "http://open.163.com/special/Khan/microeconomics.html"}; //课程地址
-	 String destinationUrl= "D:/finance/";
+			    "http://open.163.com/special/Khan/khanbiology.html",
+	    "http://open.163.com/special/Khan/physics.html"}; //课程地址
+	 String destinationUrl= "D:/science/";
 	  DownloadThread.downOpen163(COURSEURL,destinationUrl,3);
     }
 
